eff77169bcd2b2b7e09132311545c1ea91d2e17e
hafizSL2 <148432960+hafizSL2@users.noreply.github.com>
2024-01-25 14:55:26 +0800

    Update crop_recommendation_streamlit_app.py

--- crop_recommendation_streamlit_app.py
+++ crop_recommendation_streamlit_app.py
@@ -10,7 +10,7 @@ import numpy as np
 import pickle
 import matplotlib.pyplot as plt
 
-model = pickle.load(open('classifier2.pkl','rb'))
+model = pickle.load(open('classifier.pkl','rb'))
 
 b = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21]
 
